fbdcfa2597a15443d71ad8a547aa9548daa5dd0d
caicodes <caicodes1@gmail.com>
2022-08-04 22:05:27 -0600

    clean slate...

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Nuxt 3 Minimal Starter
 
+
+
 Look at the [nuxt 3 documentation](https://v3.nuxtjs.org) to learn more.
 
 ## Setup
--- app.vue
+++ app.vue
@@ -1,5 +1,21 @@
 <template>
   <div>
-    <NuxtWelcome />
+    hbo clone
   </div>
 </template>
+
+<script setup>
+useHead({
+  title: 'HBO Vue',
+  // or, instead:
+  // titleTemplate: (title) => `My App - ${title}`,
+  viewport: 'width=device-width, initial-scale=1, maximum-scale=1',
+  charset: 'utf-8',
+  meta: [
+    { name: 'description', content: 'An HBO Redesign site...' }
+  ],
+  bodyAttrs: {
+    class: 'test'
+  }
+})
+</script>
