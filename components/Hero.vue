3b8ace9c2395c2147bd54b04dfa7e6c13351e698
caicodes <caicodes1@gmail.com>
2022-08-08 21:02:44 -0600

    fixed img ref and loop

--- components/Hero.vue
+++ components/Hero.vue
@@ -1,5 +1,5 @@
 <template>
-    <section id="top" class="h-screen flex pb-40">
+    <section id="top" class="h-screen flex lg:pb-20 pb-10">
         <!-- movie treadmill container -->
         <div class="flex flex-col w-full justify-center">
             <div class="text-xl p-4 md:text-2xl text-center"> Now streaming blockbuster movies, epic originals, and
@@ -8,16 +8,17 @@
             <div class="treadmill overflow-y-auto">
                 <div id="tray-1" class="w-full mb-3 flex gap-3 -ml-28">
                     <!-- movie tiles -->
-                    <template v-for="n in 3">
+                    <template v-for="i in 3">
                         <template v-for="n in 28">
-                            <img v-if="n + 14 <= 28" :src="`/images/tiles/tile_${n + 15}.jpg`" class="w-96 rounded" />
+                            <img v-if="n + 14 <= 28" :src="`/images/tiles/tile_${n + 14}.jpg`" class="w-96 rounded" />
                         </template>
                     </template>
+
                     <img src="@/images/tiles/tile_0.jpg" class="w-96" />
                 </div>
                 <!-- movie treadmill tray two -->
                 <div id="tray-2" class="w-full mb-3 flex gap-3">
-                    <template v-for="n in 3">
+                    <template v-for="i in 3">
                         <template v-for="n in 28">
                             <img v-if="n + 14 <= 28" :src="`/images/tiles/tile_${n}.jpg`" class="w-96 rounded" />
                         </template>
